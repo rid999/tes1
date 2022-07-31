7aaf66f6a92d53aad452e3593f620a9063e47646
rid999 <110356281+rid999@users.noreply.github.com>
2022-08-01 05:27:55 +0700

    Add files via upload

---
+++ admintools_v1.sh
@@ -0,0 +1,5 @@
+###########################################################
+#this is ADMIN TOOLS PROMPT HELPER V1.0 
+#CREATED BY RID999, FREE TO USE, DONT FORGET TO CREDIT!
+#THIS TOOLS WILL EASE YOUR DAILY JOB AS SYSTEM ADMINISTRATOR
+############################################################
